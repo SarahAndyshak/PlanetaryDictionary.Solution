5958ac9fe7c2b992551fd4c333a739f195f724c6
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-03-27 09:49:25 -0700

    Add initial files and Planet.cs

---
+++ PlanetaryDictionary/Models/Planet.cs
@@ -0,0 +1,17 @@
+namespace PlanetaryDictionary.Models
+{
+  public class PlanetaryDictionary
+  {
+    public int PlanetId { get; set; }
+
+    public string Name { get; set; }
+
+    public string FunFact { get; set; }
+
+    public string Climate { get; set; }
+
+    public string LifeFormDetails { get; set; }
+
+    public int population { get; set; }
+  }
+}
